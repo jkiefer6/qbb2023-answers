59099d7f3f996f858d4de75aa872f671aa935caf
Julia <jkiefer6@jh.edu>
2023-09-06 16:14:05 -0400

    Exercise 1, step 1

--- day2-homework/plot-sisA.py
+++ day2-homework/plot-sisA.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import numpy as np
+
 import matplotlib.pyplot as plt
 
-# Get dataset to recreate Fig 3B from Lott et al 2011 PLoS Biology https://pubmed.gov/21346796
-# wget https://github.com/bxlab/cmdb-quantbio/raw/main/assignments/lab/bulk_RNA-seq/extra_data/all_annotated.csv
 
 transcripts = np.loadtxt( "all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
 print( "transcripts: ", transcripts[0:5] )
@@ -17,7 +16,7 @@ print( "data: ", data[0:5, 0:5] )
 
 # Find row with transcript of interest
 for i in range(len(transcripts)):
-    if transcripts[i] == 'FBtr0331261':
+    if transcripts[i] == 'FBtr0073461':
         row = i
 
 # Find columns with samples of interest
@@ -29,13 +28,16 @@ for i in range(len(samples)):
 # Subset data of interest
 expression = data[row, cols]
 
+
 # Prepare data
 x = samples[cols]
 y = expression
 
 # Plot data
 fig, ax = plt.subplots()
-ax.set_title( "FBtr0331261" )
+ax.set_title( "Female Data Only FBtr0073461" )
 ax.plot( x, y )
-fig.savefig( "FBtr0331261.png" )
+fig.savefig( "Female Data Only FBtr0073461.png" )
 plt.close( fig )
+
+
