022644ad022aa3ebf29b58c5ea291e841050b434
Julia <jkiefer6@jh.edu>
2023-09-06 16:30:44 -0400

    Final Part Exercise 1

--- day2-homework/plot-sisA.py
+++ day2-homework/plot-sisA.py
@@ -70,17 +70,22 @@ expression2 = data[row, cols2]
 x2 = samples[cols2]
 y3 = 2 * np.array(expression2)
 
-
+corrected = ["10", "11", "12", "13", "14A", "14B", "14C", "14D"]
 
 # Plot data
 fig, (ax) = plt.subplots()
 ax.set_title( "All Combined Data Only FBtr0073461" )
-ax.plot( x, y )
-ax.plot( x2, y2)
-ax.plot(x2, y3)
+ax.plot( corrected, y, color = 'red', label = "Female")
+ax.plot( corrected, y2, color = 'blue', label = "Male")
+ax.plot(corrected, y3, color = 'green', label = "2*Male")
+
+ax.legend()
+ax.set_xlabel("Developmental Stage")
+ax.set_ylabel("mRNA Abundance (RPKM)")
+ax.set_title("sisA")
 
 #plt.show()
-fig.savefig( "All-Combined-Data-FBtr0073461.png" )
+fig.savefig( "Final-Combined-Data-FBtr0073461.png" )
 plt.close( fig )
 
 
