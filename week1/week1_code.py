276b776204085de1600ececfe87cc9e2029136de
Julia <jkiefer6@jh.edu>
2023-09-17 16:11:01 -0400

    Step 1.2

--- week1/week1_code.py
+++ week1/week1_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pandas as pd 
 dnm = pd.read_csv('aau1043_dnm.csv')
-print(dnm)
+#print(dnm)
 
 #keys_ID = dnm.Proband_id.unique() #specifying only one indv per key
 
@@ -9,18 +9,38 @@ print(dnm)
 #when key occurs if not new append versus create new key
 #Andrew showed us how to do this earlier in the week witha  codon dict
 
-deNovoCount = {}
 
-# if key in deNovoCount.keys():
-#   deNovoCount[key].append(value)
-# else:
-#   results[key] = [value] # But here i need to change this from making list of values to returning a count
-#also will need to specify via Phase_combined which is father and mother
+#check_my_data = dnm[['Proband_id', 'Phase_combined']].copy()
+#print(check_my_data)
+#roi = (check_my_data.loc['Proband_id', :] == 675)
+#df_adelie = df.loc[roi, :]
+#print(df_adelie)
 
 
-#for individual in dnm:
-    #for every unique Proband_ID count how many times father occurs in "Phase_combined"
-    #repeat for mother
+deNovoCount = dnm.groupby('Proband_id')['Phase_combined'].apply(list).to_dict()
+#print(deNovoCount)
+
+
+
+
+for i in deNovoCount.keys():
+    #print(deNovoCount.get(i))
+    the_sum_mom = 0
+    the_sum_dad = 0
+    for values in deNovoCount.get(i):
+        if values == 'mother':
+            the_sum_mom += 1
+    for valued in deNovoCount.get(i):
+        if valued == 'father':
+            the_sum_dad += 1
+    deNovoCount[i] = [the_sum_mom, the_sum_dad]
+
+
+print(deNovoCount)
+
+#this worked I think!!
+
+
 
 
 
